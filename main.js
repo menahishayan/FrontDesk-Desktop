a109b583d56db2ff24ead4ee2c389ff312e30251
menahishayan <menahi.shayan@gmail.com>
2019-09-02 00:03:36 +0530

    Login Page UI

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const {
 	app,
-	BrowserWindow
+	BrowserWindow,
+	dialog
 } = require('electron')
 
 let win
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "http://databases.000webhost.com/db_structure.php?db=id10729791_frontdesk",
+	user: "id10729791_admin",
+	password: "nish..."
+});
+
+const options = {
+	type: 'question',
+	buttons: ['Cancel', 'Yes, please', 'No, thanks'],
+	defaultId: 2,
+	title: 'Question',
+	message: 'Do you want to do this?',
+	detail: 'Connected'
+};
 
 createWindow = () => {
 	win = new BrowserWindow({
@@ -11,12 +28,21 @@ createWindow = () => {
 		height: 600,
 		webPreferences: {
 			nodeIntegration: true
-		}
+		},
+		titleBarStyle: 'hidden'
 	})
 
 	win.loadFile('index.html')
 
 	// win.webContents.openDevTools()
+
+	// con.connect(function(err) {
+	// 	if (err) throw err;
+	// 	else dialog.showMessageBox(null, options, (response, checkboxChecked) => {
+	// 		console.log(response);
+	// 		console.log(checkboxChecked);
+	// 	});
+	// });
 
 	win.on('closed', () => {
 		win = null
