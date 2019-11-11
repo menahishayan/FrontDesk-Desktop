0999e5c6bceae9de18d65c14c05e7b05f73a3456
menahishayan <menahi.shayan@gmail.com>
2019-11-11 20:34:42 +0530

    test6

--- frontdesk.sql
+++ frontdesk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2019 at 07:18 PM
+-- Generation Time: Nov 03, 2019 at 08:05 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -38,22 +38,8 @@ CREATE TABLE `auth` (
 --
 
 INSERT INTO `auth` (`USN`, `PASSWORD`) VALUES
-('1AM17CS101', '12345678'),
-('1AM17CS102', '12345678'),
-('1AM17CS103', '12345678'),
-('1AM17CS104', '12345678'),
-('1AM17CS105', '12345678'),
-('1AM17CS106', '12345678'),
-('1AM17CS107', '12345678'),
-('1AM17CS108', '12345678'),
-('1AM17CS109', '12345678'),
-('1AM17CS110', '12345678'),
-('1AM17CS111', '12345678'),
-('1AM17CS112', '12345678'),
-('1AM17CS113', '12345678'),
-('1AM17CS121', '12345678'),
-('1AM17CS127', '12345678'),
-('1AM17CS132', '12345678');
+('1AM17CS101', 'NotPassword'),
+('1AM17CS127', 'abcde12345#');
 
 -- --------------------------------------------------------
 
@@ -72,22 +58,7 @@ CREATE TABLE `coordinators` (
 --
 
 INSERT INTO `coordinators` (`USN`, `ROLE`, `EVENT`) VALUES
-('1AM17CS101', 'core team', 'Core'),
-('1AM17CS102', 'coordinater', 'codeit'),
-('1AM17CS103', 'coordinater', 'fasion show'),
-('1AM17CS104', 'coordinater', 'group singing'),
-('1AM17CS105', 'coordinater', 'group dance'),
-('1AM17CS106', 'coordinater', 'solo singing'),
-('1AM17CS107', 'coordinater', 'solo dance'),
-('1AM17CS108', 'coordinater', 'hackaton'),
-('1AM17CS109', 'coordinater', 'treasure hunt'),
-('1AM17CS110', 'coordinater', 'beg borrow steel'),
-('1AM17CS111', 'coordinater', 'sports'),
-('1AM17CS112', 'coordinater', 'sketching'),
-('1AM17CS113', 'coordinater', 'mad ads'),
-('1AM17CS121', 'core team', 'Core'),
-('1AM17CS127', 'core team', 'Core'),
-('1AM17CS132', 'core team', 'Core');
+('1AM17CS127', 'Veg', 'Chai');
 
 -- --------------------------------------------------------
 
@@ -100,7 +71,7 @@ CREATE TABLE `events` (
   `NAME` varchar(20) NOT NULL,
   `DATE` date NOT NULL,
   `TIME` time NOT NULL,
-  `DURATION` int(11) NOT NULL,
+  `DURATION` time NOT NULL,
   `VENUE` varchar(20) NOT NULL,
   `PRICE` int(11) NOT NULL,
   `COORDINATORS` varchar(20) NOT NULL,
@@ -115,21 +86,15 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`E_ID`, `NAME`, `DATE`, `TIME`, `DURATION`, `VENUE`, `PRICE`, `COORDINATORS`, `RULES`, `CATEGORY`, `TEAM_COUNT`, `COLOR`) VALUES
-('E1001', 'Codeit', '2019-11-15', '10:00:00', 60, ' LH-201', 80, '1AM17CS102', '', 'TECHNICAL', 2, 'YELLOW'),
-('E1002', 'Fasion show', '2019-11-15', '05:00:00', 15, 'MAIN STAGE', 800, '1AM17CS103', '', 'MAIN STAGE', 8, 'RED'),
-('E1003', 'Group singing', '2019-11-15', '10:00:00', 7, 'MAIN STAGE', 400, '1AM17CS104', '', 'MAIN STAGE', 5, 'RED'),
-('E1004', 'Group dance', '2019-11-15', '11:00:00', 7, 'MAIN STAGE', 500, '1AM17CS105', '', 'MAIN STAGE', 6, 'RED'),
-('E1005', 'Solo singing', '2019-11-15', '12:00:00', 5, 'MAIN STAGE', 100, '1AM17CS106', '', 'MAIN STAGE', 1, 'RED'),
-('E1006', 'Solo dance', '2019-11-15', '01:00:00', 6, 'MAIN STAGE', 100, '1AM17CS107', '', 'MAIN STAGE', 1, 'RED'),
-('E1007', 'Hackaton', '2019-11-15', '11:00:00', 60, 'LH-202', 100, '1AM17CS108', '', 'TECHNICAL', 2, 'YELLOW'),
-('E1008', 'Treasure hunt', '2019-11-15', '12:00:00', 120, 'LH-203', 100, '1AM17CS109', '', 'OFF STAGE', 2, 'BLUE'),
-('E1009', 'Beg Borrow Steal', '2019-11-15', '01:00:00', 120, 'LH-204', 100, '1AM17CS110', '', 'OFF STAGE', 4, 'BLUE'),
-('E1010', 'Sports', '2019-11-15', '10:00:00', 60, 'MAIN GROUND', 200, '1AM17CS111', '', 'SPORTS', 11, 'GREEN'),
-('E1011', 'Sketching', '2019-11-15', '02:00:00', 60, 'LH-205', 100, '1AM17CS112', '', 'OFF STAGE', 1, 'BLUE'),
-('E1012', 'Mad ads', '2019-11-15', '03:00:00', 15, 'MAIN STAGE', 200, '1AM17CS113', '', 'MAIN STAGE', 5, 'RED'),
-('E1013', 'Quiz', '2019-11-15', '01:00:00', 60, 'LH-206', 100, '1AM17CS114', '', 'OFF STAGE', 1, 'BLUE'),
-('E1014', 'Debate', '2019-11-15', '11:00:00', 5, 'LH-207', 100, '1AM17CS115', '', 'OFF STAGE', 1, 'BLUE'),
-('E1015', 'Gaming', '2019-11-15', '12:00:00', 60, 'LH-208', 200, '1AM17CS116', '', 'OFF STAGE', 4, 'BLUE');
+('E000', 'Group Dance', '2019-09-27', '11:15:00', '07:00:00', 'Main Stage', 600, '1AM17CS127', '6+6 in a team', 'Main Stage', 4, 'Red'),
+('E001', 'Solo Singing', '2019-09-27', '12:00:00', '05:00:00', 'Main Stage', 150, '1AM17CS145', 'Time alloted 3+2 min', 'Main Stage', 1, 'Blue'),
+('E002', 'Code It', '2019-09-27', '09:30:00', '01:00:00', '201', 150, '1AM17CS005', '', 'Technical', 2, 'Purple'),
+('E003', 'Group singing', '2019-09-27', '11:15:00', '05:00:00', 'Main Stage', 600, '1AM17CS102', '6+6 in a team', 'Main Stage', 4, 'Green'),
+('E004', 'mad ads', '2019-09-27', '01:00:00', '04:00:00', 'Main Stage', 200, '1AM17CS103', 'Time alloted 3+2 min', 'Main Stage', 4, 'Orange'),
+('E005', 'hackaton', '2019-09-27', '09:30:00', '01:00:00', '201', 150, '1AM17CS006', '', 'Technical', 2, 'Purple'),
+('E006', 'solo dance', '2019-09-27', '11:15:00', '07:00:00', 'Main Stage', 200, '1AM17CS128', '6+6 in a team', 'Main Stage', 1, 'Red'),
+('E007', 'fashion show', '2019-09-27', '05:00:00', '05:30:00', 'Main Stage', 800, '1AM17CS146', 'Time alloted 3+2 min', 'Main Stage', 8, 'Blue'),
+('E008', 'quiz', '2019-09-27', '10:30:00', '01:00:00', '201', 150, '1AM17CS123', '', 'Technical', 2, 'Purple');
 
 -- --------------------------------------------------------
 
@@ -159,30 +124,16 @@ CREATE TABLE `students` (
   `DEPT` varchar(20) NOT NULL,
   `SEM` int(11) NOT NULL,
   `SECTION` varchar(20) NOT NULL,
-  `PHONE` varchar(20) NOT NULL
+  `PHONE` varchar(20) NOT NULL,
+  `EVENTS` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`USN`, `DEPT`, `SEM`, `SECTION`, `PHONE`) VALUES
-('1AM17CS101', 'cse', 5, 'b', '8971729383'),
-('1AM17CS102', 'cse', 5, 'b', '5285746350'),
-('1AM17CS103', 'cse', 5, 'b', '6325418596'),
-('1AM17CS104', 'cse', 5, 'b', '5489235489'),
-('1AM17CS105', 'cse', 5, 'b', '2041856329'),
-('1AM17CS106', 'cse', 5, 'b', '6587459856'),
-('1AM17CS107', 'cse', 5, 'b', '3254482635'),
-('1AM17CS108', 'cse', 5, 'b', '9885456565'),
-('1AM17CS109', 'cse', 5, 'b', '9875656755'),
-('1AM17CS110', 'cse', 5, 'b', '8752542585'),
-('1AM17CS111', 'cse', 5, 'b', '2535363645'),
-('1AM17CS112', 'cse', 5, 'b', '4224252555'),
-('1AM17CS113', 'cse', 5, 'b', '8747889274'),
-('1AM17CS121', 'cse', 5, 'b', '9986634653'),
-('1AM17CS127', 'cse', 5, 'b', '8660718302'),
-('1AM17CS132', 'cse', 5, 'b', '8639094896');
+INSERT INTO `students` (`USN`, `DEPT`, `SEM`, `SECTION`, `PHONE`, `EVENTS`) VALUES
+('1AM17CS127', 'CSE', 5, 'B', '2155421546', 'Chai');
 
 -- --------------------------------------------------------
 
