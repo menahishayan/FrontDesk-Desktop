78ce9fedf9733a1862a5d006a3726e2368b98cb7
Vamshi Prasad <vamvijkum@gmail.com>
2019-11-11 19:31:44 +0530

    Bug Fix

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ createWindow = () => {
 	  // loginWin.close();
 
 	  eventWin.once('show', () => {
-		db.query("SELECT E_ID,NAME,CATEGORY,COLOR FROM events ORDER BY CATEGORY", function(err, result, fields) {
+		//db.query("SELECT E_ID,NAME,CATEGORY,COLOR FROM events ORDER BY CATEGORY", function(err, result, fields) {
 		//db.query("SELECT E_ID,NAME,CATEGORY,COLOR FROM events ORDER BY CATEGORY OFFSET 1 ROW  ", function(err, result, fields) {
 			db.query("SELECT E_ID,NAME,CATEGORY,COLOR FROM events order by case when category = 'MAIN STAGE' then 0 else 1 end, category ", function(err, result, fields) {
 			if (err)
@@ -86,7 +86,7 @@ createWindow = () => {
 			})
 
 			viewEventWin.once('show', () => {
-				db.query(`SELECT * FROM events WHERE E_ID=\'${id}\'`, function(err, result, fields) {
+				//db.query(`SELECT * FROM events WHERE E_ID=\'${id}\'`, function(err, result, fields) {
 				db.query(`SELECT e.* , c.name  as a, s.phone as b FROM events e, coordinators c, students s WHERE E_ID=\'${id}\' and e.coordinators = c.usn and s.usn = e.coordinators`, function(err, result, fields) {
 					if (err)
 						dialog.showMessageBox(null, {
