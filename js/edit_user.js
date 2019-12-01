9cf7dadd51906ab2b9812bf042a35f15fcbe3e95
menahishayan <menahi.shayan@gmail.com>
2019-12-02 01:58:55 +0530

    screenshots and color fix

--- js/edit_user.js
+++ js/edit_user.js
@@ -10,6 +10,13 @@ const setValue = (field, value) => {
 
 ipcRenderer.on('edit-user', (e, userData) => {
     document.getElementById('changeForm').style.display = 'none'
+    setValue('USN',userData['USN'])
+    setValue('name',userData['NAME'])
+    setValue('sem',userData['SEM'])
+    setValue('section',userData['SECTION'])
+    setValue('dept',userData['DEPT'])
+    setValue('phone','+91 '+ userData['PHONE'])
+    setValue('role',userData['ROLE'])
 
     document.getElementById('change2').addEventListener('click', (e) => {
         e.preventDefault();
