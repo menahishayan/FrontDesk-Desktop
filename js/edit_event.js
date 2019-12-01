863318f25c3601b12d82bcf9d066bc5ae366f3e5
Vamshi Prasad <vamvijkum@gmail.com>
2019-12-01 22:44:03 +0530

    fixing time

--- js/edit_event.js
+++ js/edit_event.js
@@ -6,6 +6,7 @@
 const { ipcRenderer, remote } = require('electron')
 const db = require('./js/db')
 const showError = require('./js/showError')
+const moment = require('moment');
 
 const setValue = (field, value) => {
     document.getElementById(field).value = value;
@@ -21,22 +22,25 @@ ipcRenderer.on('edit-event', (e, eventData) => {
         setValue('category',eventData['CATEGORY'])
         setValue('venue',eventData['VENUE'])
         setValue('price',eventData['PRICE'])
-        setValue('count',eventData['COUNT'])
+        setValue('count',eventData['TEAM_COUNT'])
         setValue('coordinator',eventData['COORDINATOR'])
         color = eventData['COLOR']
+        setValue('time', moment(eventData['TIME'], "HH:mm:ss", true).format("hh:mm:ss"))
     }
 
     document.getElementById('change2').addEventListener('click', (e) => {
         e.preventDefault();
         console.log(document.getElementById("name").value );
-        //console.log(document.getElementById("time").value );
+        console.log(moment(document.getElementById("time").value , "HH:mm", true).format("hh:mm:ss A"))
+        console.log(eventData["TIME"])
+        console.log(moment(eventData['TIME'], "HH:mm:ss", true).format("hh:mm A"));
         //console.log(document.getElementById("date").value )
         //console.log(document.getElementById("duration").value)
         console.log(document.getElementById("venue").value)
         console.log(document.getElementById("price").value)
         console.log(document.getElementById("coordinator").value)
         console.log(document.getElementById("count").value)
-        console.log(document.getElementById("color").value)
+        //console.log(document.getElementById("color").value)
         console.log(document.getElementById("category").value)
         
         /*db.query(`UPDATE events SET "NAME" = ${document.getElementById("name").value} and "TIME" = ${document.getElementById("time").value} and "DATE" = ${document.getElementById("date").value} and "DURATION" = ${document.getElementById("duration").value} and "VENUE" = ${document.getElementById("venue").value} and "PRICE" = ${document.getElementById("price").value} and "COORDINATOR" = ${document.getElementById("coordinator").value} and "TEAM_COUNT" = ${document.getElementById("count").value} and "COLOR" = ${document.getElementById("color").value} and "CATEGORY" = ${document.getElementById("category").value} WHERE "E_ID" = ${eventData["E_ID"]}`, function(err, result, fields) {
