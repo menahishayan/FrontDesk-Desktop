6f03640e704a4bfc1c8cae2801ea79197c681e2f
Vamshi Prasad <vamvijkum@gmail.com>
2019-12-01 16:55:43 +0530

    return values

--- js/edit_event.js
+++ js/edit_event.js
@@ -26,5 +26,25 @@ ipcRenderer.on('edit-event', (e, eventData) => {
 
     document.getElementById('change2').addEventListener('click', (e) => {
         e.preventDefault();
+        console.log(document.getElementById("name").value );
+        //console.log(document.getElementById("time").value );
+        //console.log(document.getElementById("date").value )
+        //console.log(document.getElementById("duration").value)
+        console.log(document.getElementById("venue").value)
+        console.log(document.getElementById("price").value)
+        console.log(document.getElementById("coordinator").value)
+        console.log(document.getElementById("count").value)
+        console.log(document.getElementById("color").value)
+        console.log(document.getElementById("category").value)
+        
+        /*db.query(`UPDATE events SET "NAME" = ${document.getElementById("name").value} and "TIME" = ${document.getElementById("time").value} and "DATE" = ${document.getElementById("date").value} and "DURATION" = ${document.getElementById("duration").value} and "VENUE" = ${document.getElementById("venue").value} and "PRICE" = ${document.getElementById("price").value} and "COORDINATOR" = ${document.getElementById("coordinator").value} and "TEAM_COUNT" = ${document.getElementById("count").value} and "COLOR" = ${document.getElementById("color").value} and "CATEGORY" = ${document.getElementById("category").value} WHERE "E_ID" = ${eventData["E_ID"]}`, function(err, result, fields) {
+            if (err) showError(err)
+            else eventWin.webContents.send('events', result , isAdmin)
+        });*/
     })
+
+    
+
+    
+    
 })
